b43a7198e46a955ef62f1565b22af58b1890140d
Amrish-Reddy <76783926+Amrish-Reddy@users.noreply.github.com>
2023-03-08 21:31:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,149 @@
+var score =0;
+var gun,bluebubble,redbubble, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
+
+var redBubbleGroup, redBubbleGroup, bulletGroup;
+
+
+var life =3;
+var gameState=0
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  bulletImg = loadImage("bullet1.png")
+  blueBubbleImg = loadImage("waterBubble.png")
+  redBubbleImg = loadImage("redbubble.png")
+  backBoardImg= loadImage("back.jpg")
+  ChronoImg = loadImage("FF_Chrono.png")
+  KImg = loadImage("FF_K.png")
+  AlokImg = loadImage("Alok.png")
+  Tatsuya = loadImage("tatsuya.png")
+  Dimitri = loadImage("Dimitri.png")
+  JaiImg = loadImage("Jai.png")
+  Ak47 = loadImage("ak47.png")
+  Ac80 = loadImage("ac80.png")
+  DesertEagle = loadImage("desertEagle.png")
+  CrossBow = loadImage("crossbow.png")
+  
+}
+function setup() {
+  createCanvas(800, 800);
+
+  backBoard= createSprite(50, width/2, 100,height);
+  backBoard.addImage(backBoardImg)
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.35
+  
+  
+  bulletGroup = createGroup();   
+  blueBubbleGroup = createGroup();   
+  redBubbleGroup = createGroup();   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
+}
+
+function draw() {
+  background("#BDA297");
+  
+  
+
+  scoreboard.html("Score: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-200,20)
+
+  if(gameState===0){
+  text("Choose your Character")
+  image("")
+  }
+  if(gameState===1){
+    heading.html("Life: "+life)
+    heading.style('color:red'); 
+    heading.position(150,20)
+    gun.y=mouseY  
+
+    if (frameCount % 80 === 0) {
+      drawblueBubble();
+    }
+
+    if (frameCount % 100 === 0) {
+      drawredBubble();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+    if (blueBubbleGroup.collide(backBoard)){
+      handleGameover(blueBubbleGroup);
+    }
+    if (redBubbleGroup.collide(backBoard)) {
+      handleGameover(redBubbleGroup);
+    }
+    
+    if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }
+
+    if(redBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision(redBubbleGroup);
+    }
+
+    drawSprites();
+  }
+}
+function drawblueBubble(){
+  bluebubble = createSprite(800,random(20,780),40,40);
+  bluebubble.addImage(blueBubbleImg);
+  bluebubble.scale = 0.1;
+  bluebubble.velocityX = -8;
+  bluebubble.lifetime = 400;
+  blueBubbleGroup.add(bluebubble);
+}
+function drawredBubble(){
+  redbubble = createSprite(800,random(20,780),40,40);
+  redbubble.addImage(redBubbleImg);
+  redbubble.scale = 0.1;
+  redbubble.velocityX = -8;
+  redbubble.lifetime = 400;
+  redBubbleGroup.add(redbubble);
+}
+
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
+}
+
+function handleBubbleCollision(bubbleGroup){
+    if (life > 0) {
+       score=score+1;
+    }
+
+  //  blast= createSprite(bullet.x+60, bullet.y, 50,50);
+   // blast.addImage(blastImg)
+  //  blast.scale=0.3
+  //  blast.life=20
+    bulletGroup.destroyEach()
+    bubbleGroup.destroyEach()
+}
+
+function handleGameover(bubbleGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
+
+    if (life === 0) {
+      gameState=2
+      
+     
+    }
+  
+}
